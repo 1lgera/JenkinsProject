dffc71ca2f9f5d83bf46c5fa1ae42bbcd23aa10f
Ilya <ilogmail@mail.ru>
2025-03-05 18:02:21 +0300

    Updated

--- print_info.py
+++ print_info.py
@@ -1,3 +1,3 @@
 from datetime import datetime
 
-print(f"Logunov Ilya(with change) - Time: {datetime.now().strftime('%H:%M:%S %d-%m-%Y')}")
+print(f"Logunov Ilya - Time: {datetime.now().strftime('%H:%M:%S %d-%m-%Y')}")
